3aadf8baac7c528705b57c4d15655986242e8fff
forgeeks <forgeekscn@gmail.com>
2016-12-27 17:02:51 +0800

    更新BaseController

--- src/cn/itcast/jk/controller/BaseController.java
+++ src/cn/itcast/jk/controller/BaseController.java
@@ -16,7 +16,7 @@ import org.springframework.web.bind.annotation.InitBinder;
  */
 public abstract class BaseController {
 	@InitBinder
-	//此方法用于日期的转换，如果未加，当页面日期格式转换错误，将报400错误，实际是因为此方法
+	
 	public void initBinder(WebDataBinder binder) {
 		DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
 		dateFormat.setLenient(true);
